cde75a6a69416dfaa64b59c437f8a22eb8a0bce0
Fróði Vestergaard Dam <frodi.dam@gmail.com>
2019-11-28 11:57:27 +0100

    Started on Program

--- Program.cs
+++ Program.cs
@@ -9,17 +9,43 @@ namespace dabAs3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("DABAS3!");
+            Console.WriteLine("DABAS3!\n"
+            + "Seeding database\n\n");
 
-            var userService = new UserService();
-            List<User> list = userService.Get();
-            
-            
-
-            foreach(var l in list)
+            do
             {
-                System.Console.WriteLine(l);
+                Console.WriteLine("Welcome to FaceSlap\n\n"
+                + "Press w for wall\n"
+                + "Press f for feed\n"
+                + "Press p to create a new post\n"
+                + "Press c to create a new comment\n"
+                + "Press x to quit FaceSlap\n");
+
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "w":
+                        break;
+
+                    case "f":
+                        break;
+
+                    case "p":
+                        break;
+
+                    case "c":
+                        break;
+
+                    case "x":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command!\n\n");
+                        break;
+                }
             }
+            while(true);
         }
     }
 }
